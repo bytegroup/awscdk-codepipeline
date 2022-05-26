cf5557c5be614bb68fa5b06a6db775b5dcfbfa0a
Mizanur Rahman <mizanur.rahman@venturas-bd.com>
2022-05-26 18:13:28 +0600

    pipeline config added

--- lib/awscdk-codepipeline-stack.ts
+++ lib/awscdk-codepipeline-stack.ts
@@ -1,16 +1,22 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwscdkCodepipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwscdkCodepipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, "Pipeline", {
+      pipelineName: "CicdPipelineDemo",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub('https://github.com/bytegroup/awscdk-codepipeline', 'master'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth'
+        ]
+      }),
+    });
   }
 }
