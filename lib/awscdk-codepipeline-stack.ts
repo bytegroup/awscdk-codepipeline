ee2c2f7e88c0429596b670a86f598e1d71e59e75
Mizanur Rahman <mizanur.rahman@venturas-bd.com>
2022-05-26 18:55:12 +0600

    git config update

--- lib/awscdk-codepipeline-stack.ts
+++ lib/awscdk-codepipeline-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import {SecretValue, Stack, StackProps} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -10,7 +10,13 @@ export class AwscdkCodepipelineStack extends Stack {
     new CodePipeline(this, "Pipeline", {
       pipelineName: "CicdPipelineDemo",
       synth: new ShellStep("Synth", {
-        input: CodePipelineSource.gitHub('https://github.com/bytegroup/awscdk-codepipeline', 'master'),
+        input: CodePipelineSource.gitHub(
+            'bytegroup/awscdk-codepipeline',
+            'master',
+            {
+            authentication: SecretValue.secretsManager('github-token-cicd-pipeline-demo'),
+        }),
+
         commands: [
           'npm ci',
           'npm run build',
