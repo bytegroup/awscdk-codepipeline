326eda1799f9678fa93efb16c97d285b7e6fd846
Mizanur Rahman <mizanur.rahman@venturas-bd.com>
2022-05-27 22:59:05 +0600

    remove old code

--- lib/lambda/handler.ts
+++
@@ -1,7 +0,0 @@
-export async function handler(event: string, context: string){
-    console.log('Stage Name is ' + process.env.stage);
-    return{
-        body: 'Lambda fucntion executed - cicd codepipeline demo',
-        statusCode: 200,
-    }
-}
