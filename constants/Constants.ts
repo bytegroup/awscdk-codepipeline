f78611d5ab4b2223ff6412b18962d3a66038d540
Mizanur Rahman <mizanur.rahman@venturas-bd.com>
2022-06-09 13:58:58 +0600

    double deploy adding

--- constants/Constants.ts
+++ constants/Constants.ts
@@ -1,7 +1,11 @@
 import {Environment} from "aws-cdk-lib";
 
-export const AWS_ENV: Environment = {
+export const AWS_ENV_STG: Environment = {
     account: '361854753178',
+    region: 'ap-northeast-1'
+}
+export const AWS_ENV_PROD: Environment = {
+    account: '716993826013',
     region: 'ap-northeast-1'
 }
 
@@ -21,12 +25,19 @@ export const STAGING_WEB_CONFIG = {
     appEnv: 'staging',
 };
 
-export const APP = 'mine-map-app-client';
+export const PROD_WEB_CONFIG = {
+    baseDomainName: 'toggle-pf.com',
+    hostedZoneId: 'Z0059839IPFJ7TFOFCQH',
+    domainName: 'map-app.staging.mine.toggle-pf.com',
+    appEnv: 'production',
+};
+
+export const APP = 'mine-map-client-tmp';
 
 export const TAGS = {
     'Author': "Mizanur",
 }
 
 export const DEPLOY_IMAGE_FILE="deploy_bundle.json";
-export const HOST_BUCKET = "minemap-client-host";
-export const RESOURCE_BUCKET = "codepipeline-resources-cdk";
+export const HOST_BUCKET = "minemap-client-host-tmp";
+export const RESOURCE_BUCKET = "codepipeline-resources-cdk-tmp";
